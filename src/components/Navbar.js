fbbfb0c1d1963491c749dd8840b9485c90aafb90
Jonathanwmaddison <jonathanwmaddison@gmail.com>
2018-07-08 16:50:40 -0400

    additional improvements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,31 +9,13 @@ const Navbar = () => (
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          PH
+          Home
         </Link>
       </div>
       <div className="navbar-start">
-        <Link className="navbar-item" to="/half-blind">
-          Half Blind
-        </Link>
-        <Link className="navbar-item" to="/dad">
-          Dad
-        </Link>
         <Link className="navbar-item" to="/about">
           About
         </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
       </div>
     </div>
   </nav>
